9f92d2b056e1f265c9ed7bf6ca8a3b6b7af5bf72
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2022-09-20 10:42:50 -0300

    Initial commit

---
+++ Assets/Scripts/ReproducirAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproducirAnimation : MonoBehaviour
+{
+    public Animation animacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<Animator>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(gameObject.CompareTag("personaje"))
+        {
+            GetComponentInChildren<Animator>().enabled = true;
+        }
+    }
+
+
+}
